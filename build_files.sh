b2706bd731df025c8271c5d18c9310ed9b63fada
SATHWIK <sathwikacharya61@gmail.com>
2024-08-30 21:44:50 +0530

    pu

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,14 @@
+#!/bin/bash
 echo "BUILD START"
+
+# Install pip if not already installed
+python3.9 -m ensurepip --upgrade
 python3.9 -m pip install --upgrade pip
+
+# Install dependencies
 python3.9 -m pip install -r requirements.txt
+
+# Collect static files
 python3.9 manage.py collectstatic --noinput
+
 echo "BUILD END"
